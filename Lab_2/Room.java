86db34762dcd4ee65c4ab3ff4c35f5358b838b32
amalia001 <79217037+amalia001@users.noreply.github.com>
2022-03-03 17:35:20 +0200

    Lab_2_compulsory
    Lab_2_compulsory

---
+++ Lab_2/Room.java
@@ -0,0 +1,35 @@
+public class Room {
+    private String name;
+    private int capacity;
+    private RoomType type;
+
+   public Room(){}
+    public Room(String name, RoomType type){
+       this.name=name;
+       this.type=type;
+   }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                ", capacity=" + capacity +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ Lab_2/RoomType.java
@@ -0,0 +1,4 @@
+public enum RoomType {
+    LECTURE_HALL,
+    COMPUTER_LAB;
+}
